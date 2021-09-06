0ae8901b444a32c85fd4f4e234d38e99d2beb538
djohnson-skr <arminefendic12@gmail.com>
2021-09-06 17:48:50 -0400

    Add button functionality, clean up, comments

--- script.js
+++ script.js
@@ -2,17 +2,57 @@
 const grid = document.querySelector(".grid-container")
 const sliderValue = document.getElementById("myRange");
 const gridsize = document.querySelectorAll(".gridsize");
-let gridDimension = 0;
+const gridButton = document.querySelector(".gridButton")
+const rainbowButton = document.getElementById("rainbow");
+const blackButton = document.getElementById("black");
+const eraserButton = document.getElementById("eraser");
+let gridDimension = 25;
 
-makeGrid(25,25);
+//Set inital grid
+makeGrid(gridDimension,gridDimension);
+
+/* --- Event Listeners --- */
 document.getElementById("clear").onclick = clearGrid;
 
-// ----- display the value of the gridsize dynamicaly -----
-gridsize.forEach((size) => {
-    size.textContent = sliderValue.value;
-    gridDimension = sliderValue.value;
+gridButton.addEventListener("click", () => {
+    clearGrid();
+    /* removeGrid(); */
+    makeGrid(gridDimension,gridDimension);
 });
 
+rainbowButton.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style.background = getRandomColor();
+        });
+    });
+});
+
+blackButton.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style.background = "black";
+        });
+    });
+})
+
+eraserButton.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style.background = "rgb(185, 182, 182)";
+        });
+    });
+});
+
+// display the value of the initial gridsize 
+gridsize.forEach((size) => {
+    size.textContent = sliderValue.value;
+});
+
+// dynamicaly change the grid size numbers to match the range bar
 sliderValue.oninput = function() {
     gridDimension = sliderValue.value;
     gridsize.forEach((size) => {
@@ -20,15 +60,6 @@ sliderValue.oninput = function() {
         gridDimension = sliderValue.value;
     });
 };
-
-// ----- change the grid size via the slider and button -----
-const gridButton = document.querySelector(".gridButton")
-gridButton.addEventListener("click", () => {
-    clearGrid();
-    removeGrid();
-    makeGrid(gridDimension,gridDimension);
-});
-
 
 /*  --- Functions --- */
 function makeGrid(rows, cols) {
@@ -39,20 +70,7 @@ function makeGrid(rows, cols) {
         box.classList.add("box");
         grid.appendChild(box);
     };
-    const boxes = document.querySelectorAll(".box");
-    boxes.forEach((box) => {
-        box.addEventListener("mouseenter", () => {
-            box.style.background = "black";
-        });
-    });
 };
-
-function removeGrid() {
-    const boxes = document.querySelectorAll(".box");
-    boxes.forEach((box) => {
-        box.remove();
-    })
-}
 
 function clearGrid() {
     const boxes = document.querySelectorAll(".box");
@@ -61,10 +79,20 @@ function clearGrid() {
     });
 };
 
+function getRandomColor() {
+    let hex = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random()*16)]
+    };
+    return color;
+};
 
 
-
-
-
-
-
+/* If using borders in the grid must use removeGrid() to clear the previous border */
+/* function removeGrid() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.remove();
+    })
+} */
